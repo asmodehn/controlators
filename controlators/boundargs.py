2f8d3226b9107445d30a47d76e082b0644f79043
AlexV <asmodehn@gmail.com>
2020-08-20 17:46:47 +0200

    now boundargs represented as an AlgType

--- controlators/boundargs.py
+++ controlators/boundargs.py
@@ -6,6 +6,8 @@ import inspect
 
 import typing
 
+from .algtype import AlgType
+
 
 def frozen_arguments_type(typename: str, sig: inspect.Signature) -> typing.Type:
 
@@ -15,7 +17,7 @@ def frozen_arguments_type(typename: str, sig: inspect.Signature) -> typing.Type:
     # NamedTuple behavior is to turn non-hinted attributes into constants
     args_hints = {p: pp.annotation if pp.annotation is not inspect.Parameter.empty else typing.Any for p, pp in sig.parameters.items()}
 
-    argtype = type(f"{typename}", (typing.NamedTuple,), {
+    argtype = AlgType(f"{typename}", attrs={
         **args_defaults,
         '__annotations__': args_hints
     })
@@ -37,31 +39,19 @@ class TestHashableBoundArguments(unittest.TestCase):
         def myfun(a: int, b: float = 42.0, c = 51):
             return a, b, c
 
-        # we need something equivalent to this after the function call
-        class myfunargs(typing.NamedTuple):
-            a: int
-            b: float = 42.0  # This is a proper default (settable - py 3.8)
-            c: typing.Any = 51  # CAREFUL : without annotation, this is a default, not settable via in __new__ (py3.8)
-
         fat = frozen_arguments_type("TypeTest", sig=inspect.signature(myfun))
 
-        # Equality of named tuple class attributes
-        assert fat._fields == myfunargs._fields
-        assert fat._field_defaults == myfunargs._field_defaults
-        assert fat.__annotations__ == myfunargs.__annotations__
+        assert isinstance(fat, AlgType)
 
         # instanciation
-        myinst = myfunargs(1,2,3)
-
-        assert myinst.a == 1
-        assert myinst.b == 2
-        assert myinst.c == 3
-
         myinst_dyn = fat(1, 2, 3)
 
         assert myinst_dyn.a == 1
         assert myinst_dyn.b == 2
         assert myinst_dyn.c == 3
+
+        # the whole point
+        hash(myinst_dyn)
 
 
 if __name__ == '__main__':
